1e4bc7123cd0e139fde5644d2a0156061a3ad694
hyunjin <48453525+Hyunjin0001@users.noreply.github.com>
2022-09-05 12:08:52 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+https://discord.gg/hiruma
+
+Executer le fichier ip lookup.py, enjoy :D
+
+Rejoinds le serveur et mp moi si t'a besoin d'aide
---
+++ ip lookup.py
@@ -0,0 +1,74 @@
+import os , json
+from urllib.request import urlopen
+from colorama import Fore
+
+Choix = True
+while Choix == True:
+
+
+    ip = input("""
+┣━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━┫  
+┃                      ┃                         ┃
+┃ IP Look Up Basique   ┃ Crédit : ┃✦ Hyunjin ✧#0001┃
+┃                      ┃             ┃
+┣━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃ For [Hiruma]:   https://discord.gg/hiruma    ┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+┃
+┃ Enter Ip > """)                               
+
+    url = urlopen('https://ipinfo.io/'+ ip)
+    info = json.loads(url.read())
+
+    try:
+        ip = info['ip']
+    except:
+        ip = "Not found"
+
+    try:
+        country = info['country']
+    except:
+        country = "Not found"
+
+    try:
+        city = info['city']
+    except:
+        city = "Not found"
+
+    try:
+        region = info['region']
+    except:
+        region = "Not found"
+
+    try:
+        postal = info['postal']
+    except:
+        postal = "Not found"
+
+    try:
+        orga = info['org']
+    except:
+        orga = "Not found"
+
+    try:
+        timezone = info['timezone']
+    except:
+        timezone = "Not found"
+
+    print(f"""
+┣━━━━━━━━━━━━━━━━━━━━━━━
+┃ Ip: {ip}
+┃ Pays: {country}
+┃ Ville: {city}
+┃ Region: {region}
+┃ Code Postal {postal}
+┃ Operateur: {orga}   
+┃ TimeZone {timezone}""")
+    
+    choix = input("┣━━━━━━━━━━━━━━━━━━━━━━━\n┃ Veux tu réessayer ? [Y/N] > ")
+
+    if choix == "Y" or choix == "y":
+        Choix = True
+        
+    else:
+        Choix = False
