cebd2accf4a7496c6db3e5b4700e13ac2233bc7c
hyunjin <48453525+Hyunjin0001@users.noreply.github.com>
2023-01-01 18:04:47 -1200

    J'avoue j'ai forcer avec les couleurs ^^

--- ip lookup.py
+++ ip lookup.py
@@ -1,62 +1,101 @@
-import os , json
+import os , json    
+os.system("color E")
 from urllib.request import urlopen
 from colorama import Fore
 
 Choix = True
 while Choix == True:
+    ip = input(""" 
+
+                                $$\   $$\                                   $$\           
+                                $$ |  $$ |                                  \__|          
+                                $$ |  $$ |$$\   $$\ $$\   $$\ $$$$$$$\  $$\ $$\ $$$$$$$\  
+                                $$$$$$$$ |$$ |  $$ |$$ |  $$ |$$  __$$\ \__|$$ |$$  __$$\ 
+                                $$  __$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$\ $$ |$$ |  $$ |
+                                $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |$$ |$$ |  $$ |
+                                $$ |  $$ |\$$$$$$$ |\$$$$$$  |$$ |  $$ |$$ |$$ |$$ |  $$ |
+                                \__|  \__| \____$$ | \______/ \__|  \__|$$ |\__|\__|  \__|
+                                        $$\   $$ |              $$\   $$ |              
+                                        \$$$$$$  |              \$$$$$$  |              
+                                        \______/                \______/                                                          
 
 
-    ip = input("""
-┣━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━┫  
-┃                      ┃                         ┃
-┃ IP Look Up Basique   ┃ Crédit : ┃✦ Hyunjin ✧#0001┃
-┃                      ┃                         ┃
-┣━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━┫
-┃ For [Hiruma]:   https://discord.gg/hiruma    ┃
-┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
-┃
-┃ Enter Ip > """)                               
+Enter Ip > """)                             
+    os.system('color B')
+    os.system('color A')
+    os.system('color C')
+    os.system('color D')
+    os.system('color E')
+    os.system('color F')
+    os.system('color B')
+    os.system('color A')
+    os.system('color C')
+    os.system('color D')
+    os.system('color F')
+    os.system('color E')
+    os.system('color A')
+    os.system('color F')
+    os.system('color D')
+    os.system('color C')
+    os.system('color A')
+    os.system('color D')
+    print(f'''
 
+________  _______    ______   _______         ________  ______   _______   ________ 
+/        |/       \  /      \ /       \       /        |/      \ /       \ /        |
+$$$$$$$$/ $$$$$$$  |/$$$$$$  |$$$$$$$  |      $$$$$$$$//$$$$$$  |$$$$$$$  |$$$$$$$$/ 
+   $$ |   $$ |__$$ |$$ |  $$ |$$ |__$$ |      $$ |__   $$ |  $$ |$$ |__$$ |   $$ |   
+   $$ |   $$    $$< $$ |  $$ |$$    $$/       $$    |  $$ |  $$ |$$    $$<    $$ |   
+   $$ |   $$$$$$$  |$$ |  $$ |$$$$$$$/        $$$$$/   $$ |  $$ |$$$$$$$  |   $$ |   
+   $$ |   $$ |  $$ |$$ \__$$ |$$ |            $$ |     $$ \__$$ |$$ |  $$ |   $$ |   
+   $$ |   $$ |  $$ |$$    $$/ $$ |            $$ |     $$    $$/ $$ |  $$ |   $$ |   
+   $$/    $$/   $$/  $$$$$$/  $$/             $$/       $$$$$$/  $$/   $$/    $$/    
+
+                         ps: j'adore quand tu m'utilise :)
+''')
+
+    
+    
     url = urlopen('https://ipinfo.io/'+ ip)
     info = json.loads(url.read())
-
     try:
         ip = info['ip']
     except:
-        ip = "Not found"
+        ip = "Introuvable"
 
     try:
         country = info['country']
     except:
-        country = "Not found"
+        country = "Introuvable"
 
     try:
         city = info['city']
     except:
-        city = "Not found"
+        city = "Introuvable"
 
     try:
         region = info['region']
     except:
-        region = "Not found"
+        region = "Introuvable"
 
     try:
         postal = info['postal']
     except:
-        postal = "Not found"
+        postal = "Introuvable"
 
     try:
         orga = info['org']
     except:
-        orga = "Not found"
+        orga = "Introuvable"
 
     try:
         timezone = info['timezone']
     except:
-        timezone = "Not found"
+        timezone = "Introuvable"
 
     print(f"""
 ┣━━━━━━━━━━━━━━━━━━━━━━━
+┃ Bien jouer le h4xor 
 ┃ Ip: {ip}
 ┃ Pays: {country}
 ┃ Ville: {city}
@@ -72,3 +111,5 @@ while Choix == True:
         
     else:
         Choix = False
+    os.system('color F')    
+        
